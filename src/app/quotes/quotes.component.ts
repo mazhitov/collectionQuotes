81591330ca2f158e8a4c9fa2ccbf68ecf21370ef
Mazhitov Keneshbek <majitovkesha@gmail.com>
2021-12-11 14:53:21 +0600

    show quotes by category at home

---
+++ src/app/quotes/quotes.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { HttpService } from '../../shared/http.service';
+import { Quote } from '../../shared/quote.model';
+
+@Component({
+  selector: 'app-quotes',
+  templateUrl: './quotes.component.html',
+  styleUrls: ['./quotes.component.css']
+})
+export class QuotesComponent implements OnInit {
+  quotes: Quote[] | undefined = undefined;
+  categoryName = '';
+
+  constructor(private route: ActivatedRoute,
+              private httpService: HttpService,
+              ) {}
+
+  ngOnInit(): void {
+    this.getQuotes();
+    this.route.params.subscribe((params: Params) => {
+      this.categoryName = params['category'];
+      this.getQuotes();
+    });
+  }
+
+
+  getQuotes() {
+    if (this.categoryName) {
+      this.httpService.urlInit();
+      this.httpService.getQuotesByCategory(this.categoryName).subscribe(quotes => {
+        this.quotes = quotes;
+      })
+    } else {
+      this.httpService.urlInit();
+      this.httpService.getQuotes().subscribe(quotes => {
+        this.quotes = quotes;
+      });
+    }
+  }
+}
